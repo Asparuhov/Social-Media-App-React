72d4cb9daa77e0efeede0fcb103a57cc33b415a5
krisi01 <chrisisbeast@abv.bg>
2021-01-25 23:15:28 +0200

    fixing routing and overlay

--- client/src/containers/Main/Account/Account.js
+++ client/src/containers/Main/Account/Account.js
@@ -2,26 +2,17 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import classes from "./Account.module.css";
 import { Redirect } from "react-router-dom";
+import profileLogo from "../../../assets/profile.png";
 const Account = (props) => {
   const [currentClicked, setCurrentClicked] = useState("");
   return (
     <div className={classes.Account}>
-      <div className={classes.navbar}>
-        <button
-          onClick={() => {
-            localStorage.clear();
-            window.location.reload();
-            <Redirect to="/" />;
-          }}
-        >
-          Logout
-        </button>
-
-        <div className={classes.details}>
-          <p>ID: {props.user._id}</p>
-          <p>Email: {props.user.email}</p>
-          <p>Nickname: {props.user.username}</p>
-        </div>
+      <div className={classes.details}>
+        <img src={profileLogo} alt="default" className={classes.profilePicture} />
+        
+        <p>ID: {props.user._id}</p>
+        <p>Email: {props.user.email}</p>
+        <p>Nickname: {props.user.username}</p>
       </div>
     </div>
   );

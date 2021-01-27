96b0d8bde0a512a71e767dfb8691ee538556a5ab
krisi01 <chrisisbeast@abv.bg>
2021-01-27 23:08:09 +0200

    fixing search engine layout

--- client/src/containers/Main/Search/Search.js
+++ client/src/containers/Main/Search/Search.js
@@ -1,13 +1,17 @@
-import React, {useState, useEffect} from 'react'
-import classes from './Search.module.css';
+import React, { useState, useEffect } from "react";
+import classes from "./Search.module.css";
+import User from "./User";
 const Search = (props) => {
-    return (
-        <div className={classes.search}>
-            <input
-          placeholder="Username"
-            />
-            <button>Search</button>
-        </div>
-    )
-}
+  return (
+    <>
+      <div className={classes.search}>
+        <input placeholder="Username" />
+        <button>Search</button>
+      </div>
+      <div className={classes.users}>
+              <User />
+      </div>
+    </>
+  );
+};
 export default Search;
---
+++ client/src/containers/Main/Search/User.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import classes from './User.module.css';
+export default function User() {
+    return (
+        <div className={classes.user}>
+            
+        </div>
+    )
+}

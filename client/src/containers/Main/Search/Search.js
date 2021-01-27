ca862265d9d8024850ac86d5d85c14101c94eb29
krisi01 <chrisisbeast@abv.bg>
2021-01-27 23:01:03 +0200

    adding the search engine overlay

--- client/src/containers/Main/Search/Search.js
+++ client/src/containers/Main/Search/Search.js
@@ -1,9 +1,12 @@
 import React, {useState, useEffect} from 'react'
-
+import classes from './Search.module.css';
 const Search = (props) => {
     return (
-        <div>
-            
+        <div className={classes.search}>
+            <input
+          placeholder="Username"
+            />
+            <button>Search</button>
         </div>
     )
 }
